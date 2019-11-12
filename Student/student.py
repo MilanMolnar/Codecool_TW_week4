10677e5a00d19f70a310d08738afc768513823e0
Viktor Ferenczi <ferencziviktor11@gmail.com>
2019-11-12 12:41:55 +0100

    Update student.py

--- Student/student.py
+++ Student/student.py
@@ -27,7 +27,7 @@ def remove_student(table, id_):
     file = data_manager.get_table_from_file("Application/application.csv")
     for sublist in file:
         if sublist[2] == id_[0]:
-            raise ValueError('ID can not be deleted!')
+            ui.print_line('ID can not be deleted!')
 
     for list in table:
         for item in list:
