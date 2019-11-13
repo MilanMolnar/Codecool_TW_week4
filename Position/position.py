d6e3dc19846f849d106ca885a52c8ecbbb826b7a
Viktor Ferenczi <ferencziviktor11@gmail.com>
2019-11-13 11:55:15 +0100

    Update position.py

--- Position/position.py
+++ Position/position.py
@@ -1,54 +1,142 @@
 import ui
 import log
-import data_manager
 import common
 import data_manager
+import main
 
-log.logger.info("Student module")
-def read_student(table, ID):
-    log.logger.debug("student reading student")
+
+
+log.logger.info("Position module")
+def create_pos(table,file_name,main_list):
+    log.logger.debug("position creating position")
+    def add_pos(table, res_table):
+
+        inputs = []
+        inputs.append(common.generate_random(table))
+        for i in range(0, len(res_table)):
+            inputs.append(res_table[i])
+        table.append(inputs)
+
+        if int(inputs[2]) < 1:
+            raise ValueError
+        file = data_manager.get_table_from_file("Company/company.csv")
+        if common.is_in_table(file, inputs[-1]) is False:
+            return table
+        else:
+            ui.print_error_message("Company ID does not exist!")
+
+
+
+    table = add_pos(table, ui.get_inputs(main_list, "Please provide the following information:"))
+
+    data_manager.write_table_to_file(file_name, table)
+
+
+def read_pos(table, ID):
+    log.logger.debug("position reading position + student ids already applied")
     for i in range(len(table)):
         if table[i][0] in ID:
             ui.print_line(table[i])
+    # + students already applied are shown here
+    ui.print_line("Student IDs already applied:")
+    file = data_manager.get_table_from_file("Application/application.csv")
+    for i in range(len(file)):
+        if file[i][-1] in ID:
+            ui.print_line(file[i][-2])
+        elif file[i][-1] in ID:
+            ui.print_line("There are no students applied right now.")
 
-def change_status(table, ID_input):
-    log.logger.debug("student changing status")
+def read_positions(table):
+    '''
+
+    We print out all the positions, then we print out the position IDs, are in the application csv,
+    which means somebody applied to that position.
+
+    We print out the IDs that are not even in the application csv that means nobody applied for that position.
+
+    If the position got applies and the company accepted them(1),
+     we print out how many seats are taken in that position.
+
+    If the position got applies but the company said no to them(0),
+    we just print out "all seats are available" just like if it is not in the application csv.
+
+    '''
+
+
+    seat_list=[]
+    seat_list_count=[]
+    log.logger.debug("position reading positions + seats taken")
+    file = data_manager.get_table_from_file("Application/application.csv")
+
+
+    ui.print_line("Existing positions:\n /ID,Description,Seats,Company ID/\n")
+    for sublist in table:
+        ui.print_line(sublist)
+    ui.print_line("\n")
+
+
+    ui.print_line("Seats for positions:")
+    for sublist in table:
+        for sublist2 in file:
+            if sublist[0] == sublist2[-1]:
+                if sublist2[1] == "0":
+                    ui.print_line("ID:"+sublist[0]+" "+"All seats are available")
+                elif sublist2[1] =="1":
+                    seat_list.append(sublist[0])
+
+
+    table_ids = []
+    for i in range(len(file)):
+        table_ids.append(file[i][-1])
+
     for i in range(len(table)):
-        if table[i][0] in ID_input:
-            if table[i][-1] == '1':
-                table[i][-1] = '0'
-            else:
-                table[i][-1] = '1'
+        if table[i][0] not in table_ids:
+            ui.print_line("ID:"+table[i][0]+" "+"All seats are available")
+
+
+    duplicated_id=[]
+    one_id=[]
+    for i in range(len(seat_list)):
+        seat_list_count.append(seat_list.count(seat_list[i]))
+
+    for i in range(len(seat_list)):
+        if seat_list.count(seat_list[i]) > 1:
+            duplicated_id.append(str(seat_list[i])+"+"+str(seat_list.count(seat_list[i])))
+        elif seat_list.count(seat_list[i]) == 1:
+            one_id.append(str(seat_list[i])+"+"+str(seat_list.count(seat_list[i])))
+
+    for word in one_id:
+        ui.print_line("ID:" + word[0:word.index("+")] + " " + word[word.index("+")+1:]+" Seat is taken.")
+
+    duplicated_id = list(dict.fromkeys(duplicated_id))
+    duplicated_id = list(dict.fromkeys(duplicated_id))
+
+
+    for word in duplicated_id:
+        ui.print_line("ID:" + word[0:word.index("+")] + " " + word[word.index("+")+1:]+" Seats are taken.")
+
+    ui.print_line("\n")
+
+
+
+def update_desc(table, id_, new_desc):
+    log.logger.debug("position update description")
+    for list in range(len(table)):
+        for item in table[list]:
+            if item in id_:
+                table[list][1] = new_desc
+    ui.print_line("ID successfully updated!")
     return table
 
-def read_student(table, ID):
-    log.logger.debug("student read table")
 
-    file_app = data_manager.get_table_from_file("Application/application.csv")
-
-    for i in range(len(table)):
-        if table[i][0] in ID:
-            ui.print_line("Student: " + str(table[i]))
-
-    result = []
-    for i in range(len(file_app)):
-        if file_app[i][2] == ID[0]:
-            result.append(file_app[i])
-    if len(result) == 0:
-        ui.print_line("No application for the student!")
-    elif len(result) == 1:
-        ui.print_line("Application: " + str(result[0]))
-    else:
-        ui.print_line("Applications: " + str(result))
-
-def remove_student(table, id_):
-    log.logger.debug("student remove student")
+def remove_desc(table, id_):
+    log.logger.debug("position remove table")
 
     file = data_manager.get_table_from_file("Application/application.csv")
     for sublist in file:
-        if sublist[2] == id_[0]:
+        if sublist[-1] == id_[0]:
             ui.print_line('ID can not be deleted!')
-            raise ValueError
+            exit()
 
     for list in table:
         for item in list:
@@ -57,10 +145,10 @@ def remove_student(table, id_):
     ui.print_line("ID successfully deleted!")
     return table
 
+
+
 def start_module():
-    log.logger.debug("student starting module")
-    table = data_manager.get_table_from_file("Student/student.csv")
-    log.logger.debug("student staring module")
+    log.logger.debug("position starting module")
     menu = True
     while menu:
         handle_menu()
@@ -70,51 +158,44 @@ def start_module():
             ui.print_error_message(str(err))
 
 def choose(menu):
-    log.logger.debug("student choosing option")
-    file_name = "Student/student.csv"
+    log.logger.debug("position choosing option")
+    file_name = "Position/position.csv"
+    table = data_manager.get_table_from_file(file_name)
     inputs = ui.get_inputs(["Please enter a number: "], "")
     option = inputs[0]
-    main_list = ["Press enter to continue...", "Name: ", "Age: ", "Status(0/1): "]
-    label = ["ID", "Name", "Age", "Status"]
+    main_list = ["Description: ", "Seats: ", "Company_ID: "]
+    description = ["Description: "]
+    get_id = ["ID: "]
     if option == "1":
-        table = common.add(data_manager.get_table_from_file(file_name),
-                    ui.get_inputs(main_list, "Please provide the following information:"))
-        data_manager.write_table_to_file(file_name, table)
-    elif option == "2":
-        table = data_manager.get_table_from_file(file_name)
-        get_id = ["ID: "]
-        id_given = ui.get_inputs(get_id, "Please provide the following information:")
-        read_student(table, id_given)
-    elif option == "3":
-        common.show_table(label, data_manager.get_table_from_file(file_name))
-    elif option == "4":
-        table = common.update(data_manager.get_table_from_file(file_name),
-                       ui.get_inputs(["ID: "], "Please provide the ID to identify the student:"),
-                       ui.get_inputs(main_list, "Please provide the following information to complete the update"))
-        data_manager.write_table_to_file(file_name, table)
-    elif option == "5":
-        table = change_status(data_manager.get_table_from_file(file_name),
-                      ui.get_inputs(["ID: "], "Please provide the ID to identify the student:"))
-        data_manager.write_table_to_file(file_name, table)
-    elif option == "6":
         try:
-            get_id = ["ID: "]
-            table = data_manager.get_table_from_file(file_name)
-            id_remove = ui.get_inputs(get_id, "Please provide the following information: ")
-            remove_student(table, id_remove)
-            data_manager.write_table_to_file(file_name, table)
+            create_pos(table, file_name, main_list)
         except ValueError:
+            ui.print_line("Seat must be greater than 0!")
             start_module()
+    elif option == "2":
+        id_given = ui.get_inputs(get_id,"Please provide the following information:")
+        read_pos(table,id_given)
+    elif option == "3":
+        read_positions(table)
+    elif option == "4":
+        id_given = ui.get_inputs(get_id,"Please provide the following informations: ")
+        update_given = ui.get_inputs(description,"")
+        update_desc(table,id_given,update_given[0])
+        data_manager.write_table_to_file(file_name,table)
+    elif option == "5":
+        id_remove = ui.get_inputs(get_id,"Please provide the following information: ")
+        remove_desc(table,id_remove)
+        data_manager.write_table_to_file(file_name,table)
     elif option == "0":
         return False
+
     else:
         raise KeyError("There is no such option.")
     return True
 
 
 def handle_menu():
-    log.logger.debug("student handling menu")
-    options = ["Create student", "Read student", "Read students", "Update student","Change status", "Delete student"]
+    log.logger.debug("position handling menu")
+    options = ["Create position", "Read position","Read positions", "Update position", "Delete position"]
 
-
-    ui.print_menu("Student manager", options, "Back to main menu")
+    ui.print_menu("Position manager", options, "Back to main menu")
